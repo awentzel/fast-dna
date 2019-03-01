125411432264d9b9013902f3a515815c7db73b29
Stephane Comeau <scomea@microsoft.com>
2019-03-01 11:09:44 -0800

    working

--- packages/fast-components-react-base/src/auto-suggest/auto-suggest.spec.tsx
+++ packages/fast-components-react-base/src/auto-suggest/auto-suggest.spec.tsx
@@ -41,69 +41,47 @@ describe("select", (): void => {
         expect(rendered.first().prop("aria-label")).toEqual("label");
     });
 
-    // test("no items should be selected by default", (): void => {
-    //     const rendered: any = mount(
-    //         <Select>
-    //             {itemA}
-    //             {itemB}
-    //             {itemC}
-    //         </Select>
-    //     );
+    test("initial default state is correct", (): void => {
+        const rendered: any = mount(
+            <AutoSuggest>
+                {itemA}
+                {itemB}
+                {itemC}
+            </AutoSuggest>
+        );
 
-    //     expect(rendered.state("selectedItems").length).toBe(0);
-    // });
+        expect(rendered.state("currentValue").length).toBe(0);
+        expect(rendered.state("focusedItem")).toBe(undefined);
+        expect(rendered.state("isMenuOpen").length).toBe(false);
+    });
 
-    // test("default trigger attributes are set correctly", (): void => {
-    //     const rendered: any = mount(
-    //         <Select labelledBy="test-labelledBy">
-    //             {itemA}
-    //             {itemB}
-    //             {itemC}
-    //         </Select>
-    //     );
+    test("default input region attributes are set correctly", (): void => {
+        const rendered: any = mount(
+            <AutoSuggest label="test-label">
+                {itemA}
+                {itemB}
+                {itemC}
+            </AutoSuggest>
+        );
 
-    //     const trigger: any = rendered.find("button");
-    //     expect(trigger.prop("aria-labelledby")).toEqual("test-labelledBy");
-    //     expect(trigger.prop("aria-haspopup")).toEqual(true);
-    //     expect(trigger.prop("aria-expanded")).toEqual(false);
-    // });
+        const input: any = rendered.find("input");
+        expect(input.prop("aria-label")).toEqual("test-label");
+    });
 
-    // test("menu should open and close on select click in single mode and aria-expanded is set properly", (): void => {
-    //     const rendered: any = mount(
-    //         <Select>
-    //             {itemA}
-    //             {itemB}
-    //             {itemC}
-    //         </Select>
-    //     );
+    test("menu should open on input region click or text entry", (): void => {
+        const rendered: any = mount(
+            <AutoSuggest>
+                {itemA}
+                {itemB}
+                {itemC}
+            </AutoSuggest>
+        );
 
-    //     expect(rendered.state("selectedItems").length).toBe(0);
-    //     expect(rendered.state("isMenuOpen")).toBe(false);
-    //     expect(rendered.find("button").prop("aria-expanded")).toEqual(false);
-    //     rendered.simulate("click");
-    //     expect(rendered.state("isMenuOpen")).toBe(true);
-    //     expect(rendered.find("button").prop("aria-expanded")).toEqual(true);
-
-    //     rendered.find('[displayString="a"]').simulate("click");
-    //     expect(rendered.state("isMenuOpen")).toBe(false);
-    //     expect(rendered.state("selectedItems").length).toBe(1);
-    //     expect(rendered.find("button").prop("aria-expanded")).toEqual(false);
-    // });
-
-    // test("menu should be open by default in multi mode and should remain so during selection", (): void => {
-    //     const rendered: any = mount(
-    //         <Select multiselectable={true}>
-    //             {itemA}
-    //             {itemB}
-    //             {itemC}
-    //         </Select>
-    //     );
-    //     expect(rendered.state("isMenuOpen")).toBe(true);
-    //     expect(rendered.state("selectedItems").length).toBe(0);
-    //     rendered.find('[displayString="a"]').simulate("click");
-    //     expect(rendered.state("isMenuOpen")).toBe(true);
-    //     expect(rendered.state("selectedItems").length).toBe(1);
-    // });
+        const input: any = rendered.find("input");
+        expect(rendered.state("isMenuOpen")).toBe(false);
+        input.simulate("click");
+        expect(rendered.state("isMenuOpen")).toBe(true);
+    });
 
     // test("provided callback function is called and value changes when selection changes", (): void => {
     //     const onValueChange: any = jest.fn();
